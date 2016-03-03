b16f40e39b474180b88d536f32e39eaab4d4526f
Ryan Rishi <rdrishi12@gmail.com>
2016-03-03 09:50:03 -0800

    fixed broken path

--- campsites.sh
+++ campsites.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 env/bin/python campsites.py --start_date $1 --end_date $2 > result_$1_$2.txt
-RESULTS=`grep -E '^.*[^\n]+.*$' results/result_$1_$2.txt | wc -l`
+RESULTS=`grep -E '^.*[^\n]+.*$' result_$1_$2.txt | wc -l`
 if [ $RESULTS -gt "0" ];
 then
     echo "RESULTS FOUND - "`date`
